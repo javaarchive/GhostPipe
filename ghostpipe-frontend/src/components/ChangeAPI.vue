35bd029066ca2bc636a4a8d5751b40c333ad8b33
Raymond <raymond.peng07@gmail.com>
2022-04-14 23:16:13 +0000

    set api url

---
+++ ghostpipe-frontend/src/components/ChangeAPI.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-container fluid>
+    <div class="gp-change-api-url">
+      <h1>Change API URL</h1>
+      <p>
+        Doing this wrong may break the app. 
+      </p>
+      <v-text-field v-model="inputURL" label="api url" ></v-text-field>
+      <v-btn @click="setURL">Set and go to Homepage</v-btn>
+    </div>
+  </v-container>
+</template>
+
+<script>
+
+export default {
+  name: 'ChangeAPI',
+
+  data: () => {
+    return {
+      inputURL: ""
+    }
+  },
+  methods: {
+    setURL(){
+      localStorage.setItem("apiURL",this.inputURL);
+      this.$router.push('/');
+      // alert("api url set!");
+    }
+  },
+  mounted(){
+    this.inputURL = this.$route.params.prefill_url;
+    
+  }
+}
+</script>
