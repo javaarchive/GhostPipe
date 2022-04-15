7c12472d2a351585e1d63dcbd5ab5832cb3ee759
Raymond <20248577+javaarchive@users.noreply.github.com>
2022-04-14 22:33:30 -0700

    Update start_portable_postinstall.sh

--- start_portable_postinstall.sh
+++ start_portable_postinstall.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-npm install derhuerst/http-basic
-npm install ffmpeg-static@4.2.4
+cd ghostpipe-frontend
+echo Installing yt-dlp
+wget https://github.com/yt-dlp/yt-dlp/releases/download/2022.04.08/yt-dlp
+chmod +x yt-dlp
+cd ..
 source portable_env.sh
 ./start_backend.sh
