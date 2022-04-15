543846109b8f2d5d07ded8d505d90236c21ddd27
Raymond <20248577+javaarchive@users.noreply.github.com>
2022-04-14 22:30:30 -0700

    Update install_binaries.sh

--- install_binaries.sh
+++ install_binaries.sh
@@ -2,7 +2,8 @@
 echo Installing Binaries
 cd ghostpipe-backend
 echo Downloading FFMPEG via npm
-npm install --save-dev ffmpeg-static
+npm install derhuerst/http-basic
+npm install ffmpeg-static@4.2.4
 export USE_FFMPEG_STATIC_NPM_PKG=1
 echo Installing yt-dlp
 export YTDLP_CWD=1
